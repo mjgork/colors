7d2fe539c75696bce063914f3dd913121b2fd562
mjgork <91565727+mjgork@users.noreply.github.com>
2021-10-04 15:03:02 -0500

    Add files via upload

---
+++ mallory colors.py
@@ -0,0 +1,14 @@
+#Initialization
+import turtle
+mal = turtle.Turtle()
+import random
+
+#Functions
+def randomDot():
+    mal.color("violet")
+    mal.begin_fill()
+    mal.circle(50)
+    mal.end_fill()
+
+#Main
+randomDot()
